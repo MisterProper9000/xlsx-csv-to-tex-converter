9ad4e7ddb59b03d5be5898b58831b2d5c1b4eab7
MisterProper9000 <danya_plaksin@mail.ru>
2019-11-03 18:04:59 +0300

    Add csv converter
    See added modes in READMI

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # xlsx-cvs-to-tex-converter
 Simple excel table to tex table converter
+
+##Important
+**By now it is csv only!**
+
+##Short description:
+
+###Modes
+- "Treat empty rows as new table sign" - empty rows becomes trigger to end current table and start a new one. The number of columns is calculated for each table separately;
+- "Remove table borders" - no table border;
+- "Use latex math detection" - words containing special characters and numbers are enclosed in "$" characters. List of special characters - `latexMathList`;
+- "Table caption at bottom" - caption appears under corresponding table if checked, otherwise above the table;
+- separator selection is available when selecting a [csv file](https://en.wikipedia.org/wiki/Comma-separated_values)
+
+Latex special characters are shielded by "\". List of these characters - `latexEscapingCharacter`
+
+By default [csv quoting character](https://en.wikipedia.org/wiki/Comma-separated_values#History) is "**"**". It can only be configured through the source code.
+
+Result of  processing saves to the CONVERTER_RESILT.texCONVERTER_RESILT.tex file in the same place where the application or script lies
+
+###To-do
+- Add allowing of empty cell in table
+- Add multicolumn processing
+- Implement xlsx processing
--- converter.py
+++ converter.py
@@ -4,13 +4,15 @@ import pandas as pd
 import csv
 
 
-
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
         self.create_widgets()
+        self.latexMathList = ["\\", "^", "_"]
+        self.latexEscapingCharacter = ["#", "&"]
+        self.quotechar = '\"'
 
     def create_widgets(self):
         self.hi_there = tk.Button(self)
@@ -22,9 +24,17 @@ class Application(tk.Frame):
         self.checkBox = tk.Checkbutton(self, text="Treat empty rows as new table sign", variable=self.treatAsNew)
         self.checkBox.pack()
 
+        self.removeBorders = tk.BooleanVar()
+        self.checkBox1 = tk.Checkbutton(self, text="Remove table borders", variable=self.removeBorders)
+        self.checkBox1.pack()
 
-        self.proceed = tk.Button(self, text = "Proceed", command = self.proceed)
-        self.proceed.pack()
+        self.useMathList = tk.BooleanVar()
+        self.checkBox2 = tk.Checkbutton(self, text="Use latex math detection", variable=self.useMathList)
+        self.checkBox2.pack()
+
+        self.bottomCaption = tk.BooleanVar()
+        self.checkBox2 = tk.Checkbutton(self, text="Table caption at bottom", variable=self.bottomCaption)
+        self.checkBox2.pack()
 
         self.status = tk.Text(self,state='disabled', width=40, height=3, fg="red")
         self.status.pack()
@@ -36,12 +46,39 @@ class Application(tk.Frame):
         self.quit = tk.Button(self, text="Quit", fg="red", command=self.master.destroy)
         self.quit.pack(side="bottom")
 
+    def is_number(self, s):
+        try:
+            float(s)
+            return True
+        except ValueError:
+            return False
+
     def select(self):
-        self.master.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("xlsx files","*.xlsx"),("csv files","*.csv")))
-        self.status.configure(state='normal')
-        self.status.delete('1.0', tk.END)
-        self.status.insert(tk.END, self.master.filename, 'choosen')
-        self.status.configure(state='disabled')
+        self.master.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("xlsx files","*.xlsx"),("csv files","*.csv")))       
+        print(self.master.filename)
+        file = (self.master.filename.split("/")[-1]).split(".")
+        if len(file) > 1:
+            self.status.configure(state='normal')
+            self.status.delete('1.0', tk.END)
+            self.status.insert(tk.END, self.master.filename, 'choosen')
+            self.status.configure(state='disabled')
+
+            if file[1] == "csv":                
+                self.choiceDelimiter = tk.StringVar()
+                self.choices = ("current delimiter: \";\"", "current delimiter: \",\"")
+                self.choiceDelimiter.set(self.choices[0])
+                self.w = tk.OptionMenu(self, self.choiceDelimiter, *self.choices)
+                self.w.pack();
+
+            self.proceed = tk.Button(self, text = "Proceed", command = self.proceed)
+            self.proceed.pack()
+        else:
+            self.status.configure(state='normal')
+            self.status.delete('1.0', tk.END)
+            self.status.insert(tk.END, "No file", 'choosen')
+            self.status.configure(state='disabled')
+            self.status.tag_config('choosen', background="white", foreground="red")
+
 
     def proceed(self):
         file = (self.master.filename.split("/")[-1]).split(".")[1]
@@ -49,6 +86,11 @@ class Application(tk.Frame):
             self.proceedXLSX()
         else:
             self.proceedCSV()
+        self.status.configure(state='normal')
+        self.status.delete('1.0', tk.END)
+        self.status.insert(tk.END, "File processed. Result saved in CONVERTER_RESILT.tex", 'choosen')
+        self.status.configure(state='disabled') 
+        
 
 
     def proceedXLSX(self):        
@@ -62,49 +104,122 @@ class Application(tk.Frame):
 
         f = open('CONVERTER_RESILT.tex','w')
         tableCounter = 1
-        f.write("\\begin{table}[H]\n\\caption{"+name+str(tableCounter)+"}\n\\label{tab:my_label"+str(tableCounter)+"}\n\\begin{center}\n\\vspace{5mm}\n\\begin{tabular}{|")
-
-        for i in range(data.shape[1]):
-            f.write("c|")
-        f.write("}\n\\hline\n")
-
+        f.write("\\begin{table}[H]\n"+("\\caption{"+name+str(tableCounter)+"}\n" if self.bottomCaption.get() == False else "")+"\\label{tab:my_label"+str(tableCounter)+"}\n\\begin{center}\n\\vspace{5mm}\n\\begin{tabular}{" + ("|" if (self.removeBorders.get() == False) else ""))
+        
         emptyLinesListener = ""
-        actualColumnNum = data.shape[1] #csv.reader calculates number of columns by first row, but it could be uncorrect, so this value will be calculated after string parsing
+        actualColumnNum = {}
+        actualColumnNum[tableCounter] = data.shape[1] #csv.reader calculates number of columns by first row, but it could be uncorrect, so this value will be calculated after string parsing
+        
 
-        with open(self.master.filename, "r") as my_input_file:
-            for row in csv.reader(my_input_file):
+        delimiter = self.choiceDelimiter.get().split("\"")[1]
+        counter = 0;
+        needNewTable = False
+        with open(self.master.filename, "r") as my_input_file: #calculate number of columns
+            for row in csv.reader(my_input_file, delimiter=delimiter, quotechar = self.quotechar):
+                #print(row)
+                counter = 0;
+                if (len(row) == 0 and self.treatAsNew.get() == True):#if empty line is a table delimiter then no need to count column count across all rows
+                    needNewTable = True
+                    continue
+
+                if (needNewTable):
+                    tableCounter += 1
+                    actualColumnNum[tableCounter] = 0
+                    needNewTable = False
+
+                for entry in row:
+                    if len(entry.strip()) > 0:
+                        counter = counter + 1
                 
-                if ((" ".join(row).replace(" ","") == "" or " ".join(row).replace(" ","") == "\n") and self.treatAsNew.get() == True):
-                    emptyLinesListener = "1"
-                else:
-                    if(emptyLinesListener != "1"):
-                        string = (" ".join(row)).rstrip(';').replace(";", "&")
-                        actualColumnNum = max(actualColumnNum, string.count("&")+1)
-                        if string.count("&")+1 < actualColumnNum:
-                            string+=(actualColumnNum-1-string.count("&"))*" &"
-                            string+="\n"
-                        string+="\\\\\n\\hline\n"
-                        f.write(string)
-                    else:
+                actualColumnNum[tableCounter] = max(actualColumnNum[tableCounter], counter)
+                #print(tableCounter)
+                #print(actualColumnNum[tableCounter])
+                #print("______")
+
+        tableCounter = 1
+        for i in range(actualColumnNum[tableCounter]-1):
+            f.write("c|")
+        f.write("c") if self.removeBorders.get() == True else f.write("c|")
+        f.write("}\n\\hline\n") if self.removeBorders.get() == False else f.write("}\n")
+        #print(len(actualColumnNum))
+        
+        
+        needNewTable = False
+        row_counter = 0
+        
+        with open(self.master.filename, "r") as my_input_file:
+            cvs_reader = csv.reader(my_input_file, delimiter=delimiter, quotechar = self.quotechar)
+            for row in cvs_reader:
+                #print(row)
+                row_counter = row_counter + 1
+                if (len(row) > 0):
+                    if (needNewTable == True):
                         tableCounter += 1
-                        string = "\\end{tabular}\n\\end{center}\n\\end{table}\n\n\\begin{table}[H]\n\\caption{"+name+str(tableCounter)+"}\n\\label{tab:my_label"+str(tableCounter)+"}\n\\begin{center}\n\\vspace{5mm}\n\\begin{tabular}{|"+actualColumnNum*"c|"+"}\n\\hline\n"+(" ".join(row)).rstrip(';').replace(";", "&")
-                        if string.count("&")+1 < actualColumnNum:
-                            string+=(actualColumnNum-1-string.count("&"))*" &"
-                            string+="\n"
-                        string+="\\\\\n\\hline\n"
-                        f.write(string)
-                        emptyLinesListener = ""
+                        if (self.removeBorders.get() == False):
+                            f.write("\n\\hline\n")
+                        else:
+                            f.write("\n")
+                        f.write("\\end{tabular}\n"+("\\caption{"+name+str(tableCounter-1)+"}\n" if self.bottomCaption.get() == True else "")+"\\end{center}\n\\end{table}\n\n\\begin{table}[H]\n"+("\\caption{"+name+str(tableCounter)+"}\n" if self.bottomCaption.get() == False else "")+"\\label{tab:my_label"+str(tableCounter)+"}\n\\begin{center}\n\\vspace{5mm}\n\\begin{tabular}{"+("|" if self.removeBorders.get() == False else "")+(actualColumnNum[tableCounter]-1)*"c|"+("c|" if self.removeBorders.get() == False else "c")+"}\n"+("\\hline\n" if self.removeBorders.get() == False else ""))
+                        needNewTable = False
+                    else:
+                        if(row_counter > 1):
+                            f.write("\\hline\n")
+                    i = 0
+                    for entry in row:
 
-                   
-            
-       
+                        entry = entry.strip()
+                        if(len(entry) == 0):
+                            continue
+                        i += 1
 
-            f.write("\\end{tabular}\n\\end{center}\n\\end{table}")
+                        for char in self.latexEscapingCharacter:
+                            entry = entry.replace(char, "\\" + char)
 
-        f.close() 
-        print(self.treatAsNew.get())
+                        if (self.useMathList.get() == True):
+                            sub_entries = entry.split(" ")
+                            for index, sub_entry in enumerate(sub_entries):
+                                if (self.is_number(sub_entry) == True):
+                                    sub_entries[index] = "$" + sub_entry
+                                    j = 1
+                                    while(index + j < len(sub_entries) and self.is_number(sub_entries[j]) == True):
+                                        sub_entries[index] += " " + sub_entries[index + j]
+                                        sub_entries[index + j] = ""
+                                        j = j + 1
+                                    sub_entries[index] += "$"
+                                    index += j - 1
+                                    continue
+                                else:
+                                    for math in self.latexMathList:
+                                        if (sub_entry.find(math) != -1):
+                                            sub_entries[index] = "$" + sub_entries[index] + "$"
+                                            break
+
+                            entry = ""
+                            for sub_entry in sub_entries:
+                                entry += sub_entry
+
+                        f.write(entry + (" & " if (i != (actualColumnNum[tableCounter])) else ""))
+                    
+                    f.write((actualColumnNum[tableCounter]-i-1)*"& ")
+                    f.write("\\\\")
+                    f.write("\n")
+                else:
+                    if (self.treatAsNew.get() == True):
+                        needNewTable = True
+                    else:
+                        f.write("\n\\hline\n")
+                        f.write((actualColumnNum[tableCounter] - 1) * "& ")
+                        f.write("\\\\\n")
+
+            if (self.removeBorders.get() == False):
+                f.write("\n\\hline\n")
+            else:
+                f.write("\n")
+            f.write("\\end{tabular}\n"+("\\caption{"+name+str(tableCounter)+"}\n" if self.bottomCaption.get() == True else "")+"\\end{center}\n\\end{table}")
+
+        f.close()
 
 root = tk.Tk()
-root.title("XLSX/CVS to TeX converter")
+root.title("XLSX/CSV to TeX converter")
 app = Application(master=root)
 app.mainloop()
